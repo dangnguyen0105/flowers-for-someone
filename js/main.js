58afba6c10d7b7516c940d7141c7b06e0c3a1718
dangnguyen0105 <weangdeang0105@gmail.com>
2025-06-15 23:44:47 +0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@ onload = () => {
   const c = setTimeout(() => {
     document.body.classList.remove("not-loaded");
 
-    const titles = ('I LOVE U').split('')
+    const titles = ('Bạn Luna ngủ ngoann nhée').split('')
     const titleElement = document.getElementById('title');
     let index = 0;
 
